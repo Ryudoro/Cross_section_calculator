21cb8c14278297c2231fac0fa7958f89d5137fce
Ryudoro <93795941+Ryudoro@users.noreply.github.com>
2023-04-18 21:44:13 +0200

    Update Controller.py

--- core/calculation/Controller.py
+++ core/calculation/Controller.py
@@ -1,5 +1,10 @@
 import subprocess
 import os
+import json
+import sys
+sys.path.append('Cross_section_calculator')
+
+from Parameters.File_creation.SLHA_input import extract_m1_m2_mu
 
 def extract_softsusy_folder(file, outputfile):
   commande = f'tar -xzf {file} -C {outputfile}
@@ -8,3 +13,10 @@ def extract_softsusy_folder(file, outputfile):
 def is_there_folder(dossier):
   return os.path.isdir(dossier)
     
+def neutralino_choice(file):
+  m1,m2,mu = extract_m1_m2_mu(file)
+  with open('infos.json', 'r') as file:
+    data = json.load(file)
+    parameters = data['evenements'][
+    
+  
