31686ba9a0f35326a891a0c9dbcb2a468c75f362
Ryudoro <93795941+Ryudoro@users.noreply.github.com>
2023-04-18 19:11:00 +0200

    first controler's functions

--- core/calculation/Controller.py
+++ core/calculation/Controller.py
@@ -1,1 +1,10 @@
-print("every thing is under controle")
+import subprocess
+import os
+
+def extract_softsusy_folder(file, outputfile):
+  commande = f'tar -xzf {file} -C {outputfile}
+  
+  
+def is_there_folder(dossier):
+  return os.path.isdir(dossier)
+    
