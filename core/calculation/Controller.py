ee748c427bbfb956b83b76c1b9efe37e60588d52
Ryudoro <93795941+Ryudoro@users.noreply.github.com>
2023-04-19 09:32:21 +0200

    Update Controller.py

--- core/calculation/Controller.py
+++ core/calculation/Controller.py
@@ -44,9 +44,45 @@ def neutralino_choice(file):
           return json_data
               
 def routine():
-  input_dir = ../../Parameters/Data
+  input_dir = "../../Parameters/Data"
+  resummino_input = os.path.join(input_dir, "resummino_input")
   extract_softsusy_folder(os.path.join(input_dir, "slha_folder.tar"), os.path.join(input_dir, "slha_folder"))
   liste_input = os.listdir(input_dir)
+  os.makedirs(resummino_input)
   for input in liste_input:
       particles = neutralino_choice(input)
+
+def routine_creation():
+  input_dir = "../../Parameters/Data"
+  resummino_folder = os.path.join(input_dir, "resummino_input")
+  extract_softsusy_folder(os.path.join(input_dir, "slha_folder.tar"), os.path.join(input_dir, "slha_folder"))
+  liste_input = os.listdir(os.path.join(input_dir, "slha_folder"))
+  os.makedirs(resummino_folder)
+  tasks = []
+  for input in liste_input:
+      particles = neutralino_choice(os.path.join(input_dir, "slha_folder",input))
+      particles = json.loads(particles)
+      for particle_pair in particles["liste_particles"]:
+        outgoing_particle_1 = particle_pair["outgoing_particle_1"]
+        outgoing_particle_2 = particle_pair["outgoing_particle_2"]
+        inpute = input.rstrip(".slha")
+        resummino_input = f"resummino_{outgoing_particle_1}_{outgoing_particle_2}_{inpute}.in"
+        modifie_outgoing_particles("resummino_modified.in", os.path.join(resummino_folder,resummino_input), outgoing_particle_1, outgoing_particle_2)
+        modifie_slha_file(resummino_input, resummino_input, os.path.join("output_dir",input))
+        output_resummino = os.makedirs(os.path.join(input_dir, "output_resummino",f"resummino_{outgoing_particle_1}_{outgoing_particle_2}_{inpute}.txt" ))
+        tasks.append(resummino_input, output_resummino)
+   return tasks
+
+def run_resummino(input_file, output_file):
+    #modifie_slha_file(input_file, slha_file)
+    commande = f"{./../../../resummino-releases/bin/resummino} {input_file}"
+    with open(output_file, 'w') as f:
+        subprocess.run(commande, shell=True, stdout=f, text=True)
+        
+def routine_resummino():
+  tasks = routine_creation()
+  with ProcessPoolExecutor() as executor:
+    futures = [executor.submit(run_resummino, *task) for task in tasks]
+    for future in futures:
+        future.result()
       
