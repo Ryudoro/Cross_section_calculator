5cb6d153915601a344036a2b87b6e27e634f720b
Ryudoro <93795941+Ryudoro@users.noreply.github.com>
2023-04-26 09:19:44 +0200

    Update creation.py

--- core/calculation/creation.py
+++ core/calculation/creation.py
@@ -9,6 +9,7 @@ sys.path.append(ancre)
 from Parameters.File_creation.Resumino_input.Outgoing_particles import modifie_outgoing_particles
 from Parameters.File_creation.Resumino_input.slha_file_change import modifie_slha_file
 from core.calculation.neutralino import neutralino_choice
+
 def extract_softsusy_folder(file, outputfile):
   commande = f'tar -xzf {file} -C {outputfile}'
   subprocess.run(commande, shell=True, text=True)
@@ -22,19 +23,24 @@ def routine_creation():
   data_dir = os.path.join(parameters_dir, 'Data')
   if True:
     extract_softsusy_folder(os.path.join(parameters_dir,'File_creation','SLHA_input', "softsusy_output.tar"), data_dir)
-  liste_input = os.listdir(os.path.join(input_dir, "slha_folder"))
-  os.makedirs(resummino_folder)
+  liste_input = os.listdir(os.path.join(input_dir, "output_dir"))
+  if not os.path.exists(resummino_folder):
+    os.makedirs(resummino_folder)
   tasks = []
   for input in liste_input:
-      particles = neutralino_choice(os.path.join(input_dir, "slha_folder",input))
+      particles = neutralino_choice(os.path.join(input_dir, "output_dir",input))
       particles = json.loads(particles)
       for particle_pair in particles["liste_particles"]:
         outgoing_particle_1 = particle_pair["outgoing_particle_1"]
         outgoing_particle_2 = particle_pair["outgoing_particle_2"]
         inpute = input.rstrip(".slha")
         resummino_input = f"resummino_{outgoing_particle_1}_{outgoing_particle_2}_{inpute}.in"
-        modifie_outgoing_particles("resummino_modified.in", os.path.join(resummino_folder,resummino_input), outgoing_particle_1, outgoing_particle_2)
-        modifie_slha_file(resummino_input, resummino_input, os.path.join("output_dir",input))
-        output_resummino = os.makedirs(os.path.join(input_dir, "output_resummino",f"resummino_{outgoing_particle_1}_{outgoing_particle_2}_{inpute}.txt" ))
-        tasks.append(resummino_input, output_resummino)
+        resummino_path = os.path.join(resummino_folder, resummino_input)
+        print(resummino_input)
+        modifie_outgoing_particles(os.path.join(data_dir,"resummino_modified.in"), resummino_path, outgoing_particle_1, outgoing_particle_2)
+        file_path  = os.path.join(input_dir, "output_resummino",f"resummino_{outgoing_particle_1}_{outgoing_particle_2}_{inpute}.txt" )
+        modifie_slha_file(resummino_path, resummino_path, os.path.join("output_dir",input))
+        if not os.path.exists(file_path):
+          output_resummino = os.makedirs(file_path)
+        tasks.append((resummino_path, file_path))
   return tasks
