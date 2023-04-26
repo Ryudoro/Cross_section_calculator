236441da248e45df137048a287a539c41a5f9e96
Ryudoro <93795941+Ryudoro@users.noreply.github.com>
2023-04-26 09:19:11 +0200

    Update neutralino.py

--- core/calculation/neutralino.py
+++ core/calculation/neutralino.py
@@ -1,16 +1,18 @@
 import json
 import sys
+import os
 from ancre_search import trouver_chemin_ancre
 
 ancre = trouver_chemin_ancre("README.md")
-sys.path.append('ancre')
+sys.path.append(ancre)
 
 from Parameters.File_creation.SLHA_input.exctract_param import extract_m1_m2_mu
 
 def parameter_study():
     param_study = []
     param_name = []
-    with open('infos.json', 'r') as file:
+    json_path = os.path.join(ancre, 'core', 'calculation', 'infos.json')
+    with open(json_path, 'r') as file:
         data = json.load(file)
         parameters = data['evenements'][1]['parametres']
     for param in list(parameters):
@@ -20,9 +22,9 @@ def parameter_study():
     return param_study,param_name
 
 def neutralino_choice(file):
+    json_path = os.path.join(ancre, 'core', 'calculation', 'infos.json')
     m1,m2,mu = extract_m1_m2_mu(file)
     param_study, param_name = parameter_study()
-    m2 = 1000
     if 'mu' and 'M2' in param_name:
        if float(m2) > float(mu):
           json_data = '{"liste_particles" : [{"outgoing_particle_1": 1000025, "outgoing_particle_2": 1000037}, {"outgoing_particle_1": 1000025, "outgoing_particle_2": -1000037}, {"outgoing_particle_1": -1000037, "outgoing_particle_2": 1000037}]}'
@@ -36,3 +38,4 @@ def neutralino_choice(file):
           with open('particles.json', 'w') as file:
               json.dump(data, file)
           return json_data
+
